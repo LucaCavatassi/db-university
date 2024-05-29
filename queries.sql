dbd3fd4becfcc3a74151d190775d88aa466fb2e6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-29 15:22:30 +0200

    feat: 4-5-6

--- queries.sql
+++ queries.sql
@@ -18,3 +18,19 @@ SELECT *
 FROM `courses`
 WHERE `period` = "I semestre" 
 AND `year` = 1;
+
+-- ______________________
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * 
+FROM `exams`
+WHERE `date` = "2020-06-20"
+AND `hour`> "14:00:00";
+
+-- Selezionare tutti i corsi di laurea magistrale
+SELECT * 
+FROM `degrees`
+WHERE `level` = "magistrale";
+
+-- Da quanti dipartimenti è composta l'università?
+SELECT COUNT(`id`)
+FROM `departments`;
