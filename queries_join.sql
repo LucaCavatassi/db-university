156a3a9719b2c1d20ae9b19367de0213e0798fb1
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-30 16:43:11 +0200

    feat: join 1-2-3-4

--- queries_join.sql
+++ queries_join.sql
@@ -12,3 +12,21 @@ INNER JOIN `departments`
 ON `degrees`.`department_id`=`departments`.`id`
 WHERE `degrees`.`level` = "magistrale"
 AND `departments`.`name`= "Dipartimento di Neuroscienze";
+
+-- Selezionare tutti i corsi in cui insegna Fulvio Amato
+SELECT `courses`.`name`, `teachers`.`name`, `teachers`.`surname`
+FROM `course_teacher` 
+INNER JOIN `courses`
+ON `course_teacher`.`course_id` = `courses`.`id`
+INNER JOIN `teachers`
+ON `course_teacher`.`teacher_id`= `teachers`.`id`
+WHERE `teachers`.`id`= 44;
+
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`, `departments`.`name` 
+FROM `students`
+INNER JOIN `degrees`
+ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+ON `departments`.`id`=`degrees`.`department_id`
+ORDER BY `students`.`name`,`students`.`surname`;
